d1ee96fa4b13799581a74b50672114cbb636a1c5
TAKO-zhangyu <takoforme@gmail.com>
2021-09-06 23:09:27 +0900

    formの位置を中心に設定

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,10 +1,19 @@
 import React from 'react';
 
+let style = {
+  textAlign: 'center',
+};
+
+let button = {
+  marginLeft: '1%',
+};
+
 const Form = (props) => (
-  <form className="siimple-form">
-    <div className="siimple-form-field">
+  <form className="siimple-form" onSubmit={props.handleAdd}>
+    <div className="siimple-form-field" style={style}>
       <label className="siimple-label siimple--color-white">Your todo:</label>
-      <input name="title" type="text" className="siimple-input"/><input type="submit" value="Add" className="siimple-btn siimple-btn--teal"/>
+      <input name="title" type="text" className="siimple-input"/>
+      <input type="submit" value="Add" className="siimple-btn siimple-btn--teal" style={button}/>
     </div>
   </form>
 );
