353098c34fc5d44915f5b0b2ac7d751a7bb892aa
TAKO-zhangyu <takoforme@gmail.com>
2021-12-24 22:47:41 +0900

    関数型移植途中

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,37 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Form from './Form';
 import List from  './List';
 
-export default class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      todo: []
-    };
-    this.handleAdd = this.handleAdd.bind(this);
-    this.handleRemove = this.handleRemove.bind(this);
-    this.handleUpdate = this.handleUpdate.bind(this);
-  }
+const App = () => {
+  const [todo, setTodo] = useState([]);
 
-  // データ保存
-  handleAdd(e){
-    // リダイレクト防止
-    e.preventDefault();
-    // フォームから受け取ったデータをオブジェクトに挿入して、stateのtodo配列に追加
-    this.state.todo.push({title: e.target.title.value}); // まだ保存されていない
-    // setStateを使ってstateを上書き
-    this.setState({todo: this.state.todo}); // 保存完了
-    // inputのvalueを空に
-    e.target.title.value = '';
-  }
+  const handleAdd = useCallback(
+    // e.preventDefault();
+    (e) => {
+      setTodo(e.target.title.value);
+    },
+    [setTodo]
+    // e.target.title.value = '';
+  );
 
-  // データ削除
-  handleRemove(i){
-    // todo配列からi番目から1つ目のデータを除外
-    this.state.todo.splice(i,1);
-    // setStateでtodo配列を上書き
-    this.setState({todo: this.state.todo});
-  }
+  const handleRemove = (i) => {
+    // useState({title: e.target.title.value});
+    setTodo({todo: todo.splice(i,1)});
+  };
 
   // データ更新
   handleUpdate(text, i){
@@ -39,18 +25,19 @@ export default class App extends Component {
     this.setState({todo: this.state.todo});
   }
 
-  render() {
-    return (
-      <div className="siimple-box siimple--bg-dark">
-        <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
-        <Form handleAdd={this.handleAdd}/>
-        <div className="siimple-rule"></div>
-        <List 
-          todos={this.state.todo}
-          handleRemove={this.handleRemove}
-          handleUpdate={this.handleUpdate}
-        />
-      </div>
-    );
-  }
+
+  return (
+    <div className="siimple-box siimple--bg-dark">
+      <h1 className="siimple-box-title siimple--color-white">React Todo App</h1>
+      <Form handleAdd={this.handleAdd}/>
+      <div className="siimple-rule"></div>
+      <List 
+        todos={this.state.todo}
+        handleRemove={this.handleRemove}
+        handleUpdate={this.handleUpdate}
+      />
+    </div>
+  );
 }
+
+export default App
